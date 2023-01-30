4f9f4ba7a64d7912d56a98365ec2e539d636ec3f
Julia Mendes <juliamendesc@gmail.com>
2023-01-30 14:08:06 +0000

    fix tsconfig import problem

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*': 'prettier --ignore-unknown --write',
-  '*.{js,ts,jsx,tsx}': 'eslint --config .eslintrc.json --fix',
+  '*.{js,ts,jsx,tsx}': 'eslint --config .eslintrc.js --fix',
 };
