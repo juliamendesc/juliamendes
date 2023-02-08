41f4406262f6f546ae355c01cff4ab1349947297
Julia Mendes <juliamendesc@gmail.com>
2023-02-08 22:34:16 +0000

    fix responsivity in project section

--- components/projects/ProjectsSection.tsx
+++ components/projects/ProjectsSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProjectCard, { ProjectProps } from './ProjectCard';
 import styles from './ProjectsSection.module.css';
 import SectionTitle from 'components/section-title/SectionTitle';
+import Link from 'next/link';
 
 type ProjectsArrayProps = {
   projects: ProjectProps[];
@@ -16,6 +17,18 @@ const ProjectsSection = (projects: ProjectsArrayProps) => {
           <ProjectCard key={project.id} {...project} />
         ))}
       </section>
+
+      <div className={styles.otherProjectsWrapper}>
+        <button className={styles.otherProjects}>
+          <Link
+            href="https://github.com/juliamendesc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Want to see more? Checkout my Github!
+          </Link>
+        </button>
+      </div>
     </>
   );
 };
