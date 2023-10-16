28ecff29470ce483d8900d016aa8cc79e7308d94
Julia Mendes <juliamendesc@gmail.com>
2023-10-16 22:55:12 +0100

    fix lint error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import PageNotFound from '../components/page-not-found/PageNotFound';
+import PageNotFound from 'components/page-not-found/PageNotFound';
 
 export const PageNotFoundPage: FC = () => {
   return <PageNotFound />;
